016d74531da320a463f538040ba15901ae0f17ad
Briana Bennett <bri.bennett89@gmail.com>
2017-09-13 13:15:57 -0700

    all routes created

--- apps/private/urls.py
+++ apps/private/urls.py
@@ -1,4 +1,10 @@
 from django.conf.urls import url
-from . import views           
-urlpatterns = [     
+from . import views 
+
+urlpatterns = [ 
+	url(r'^home_2$', views.home_2),
+	url(r'^features$', views.features),
+	url(r'^expertise$', views.expertise), 
+	url(r'^partner$', views.partner),
+	url(r'^about$', views.about)   
 ]
--- apps/private/views.py
+++ apps/private/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+def home_2(request):
+	return render(request, 'private/home_2.html')
+
+def features(request):
+	return render(request, 'private/features.html')
+
+def expertise(request):
+	return render(request, 'private/expertise.html')
+
+def partner(request):
+	return render(request, 'private/partner.html')
+
+def about(request):
+	return render(request, 'private/about.html')
